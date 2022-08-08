a280df50df4288d23af4ee318b6811c3ec3f07df
redital <86683344+redital@users.noreply.github.com>
2022-08-08 02:19:15 +0200

    Aggiunta movimenti random NPC
    manca però di aggiugere un controllo sulla distanza

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class NPC : Mover
 {
-    
+    public float moveSpeed = 0.08f;
+    public float minTime=1.0f, maxTime=4.0f;
+    internal float decisionTimeCount = -1.0f;
+
+    // Possibili direzioni in cui l'oggetto può muoversi. Zero c'è due volte per aumentare la probabilità che stia fermo
+    internal Vector3[] moveDirections = new Vector3[] { new Vector3(1.0f,0,0), new Vector3(-1.0f,0,0), new Vector3(0,1.0f,0), new Vector3(0,-1.0f,0), new Vector3(0,0,0), new Vector3(0,0,0) };
+    internal int currentMoveDirection = 0;
+
+    void Update()
+    {
+
+        // Bisogna aggiungere che se supera una certa distanza dalla posizione di partenza allora devve tornare indietro
+        // la soluzione più semplice a livello di codice probabilmente è se è lontano e questa sclta lo farà allontanare ancora allora cambia scelta
+
+        // Faccio muovere l'oggetto
+        UpdateMotor(moveDirections[currentMoveDirection]* moveSpeed);
+
+        if (decisionTimeCount > 0) {
+            decisionTimeCount -= Time.deltaTime;
+        }
+        else{
+            // Setto il tempo per cui durerà la prossima mossa
+            decisionTimeCount = Random.Range(minTime, maxTime);
+ 
+            // Scelgo la direzione in cui si muoverà per i prossimi decisionTimeCount secondi
+            ChooseMoveDirection();
+
+            
+        }
+        
+    }
+ 
+    void ChooseMoveDirection()
+    {
+        // Scelgo randomicamente la direzione tra quelle impostate
+        currentMoveDirection = Mathf.FloorToInt(Random.Range(0, moveDirections.Length));
+    }
 }
