7902b0b893bb0ad1e9cb28a908355e4e8333df6e
redital <86683344+redital@users.noreply.github.com>
2022-09-11 18:51:35 +0200

    fix e cambio impostazione per il build

--- Assets/Script/Talker.cs
+++ Assets/Script/Talker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
-using UnityEditor.VersionControl;
+//using UnityEditor.VersionControl;
 
 public class Talker : Collidable
 {
