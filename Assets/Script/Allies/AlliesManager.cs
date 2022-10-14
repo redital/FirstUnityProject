b80049f560574b2ac37b94f029265a1970446c29
Barbarezza <nico.meduri@gmail.com>
2022-10-14 18:10:42 +0200

    Added cheats terminal. Completed logic to add an ally to the allies list

--- Assets/Script/Allies/AlliesManager.cs
+++ Assets/Script/Allies/AlliesManager.cs
@@ -1,19 +1,23 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
+using CommandTerminal;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class AlliesManager : MonoBehaviour
 {
+    public static AlliesManager Instance { get; private set; }
+
     public FightersListSO AllPossibleAllies;
 
     [Space(10)]
-
+    
     public int MaxAlliesNumberAvailable = 10;
     
-    private List<Ally> ActiveAllies = new();
-    private List<Ally> NotActiveAllies = new ();
+    [SerializeField]
+    private List<Alleato> ActiveAllies = new();
+    [SerializeField]
+    private List<Alleato> NotActiveAllies = new ();
 
     [Space(10)]
 
@@ -25,6 +29,15 @@ public class AlliesManager : MonoBehaviour
 
     private void Awake()
     {
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        } 
+        
         this.saveDirectory = $"{Application.persistentDataPath}/SaveFiles";
         this.savePath = $"{saveDirectory}/AlliesData.json";
         
@@ -45,7 +58,7 @@ public class AlliesManager : MonoBehaviour
         }
     }
 
-    public void RemoveAllyFromActiveAllies(Ally allyToRemove)
+    public void RemoveAllyFromActiveAllies(Alleato allyToRemove)
     {
         this.ActiveAllies.Remove(allyToRemove);
         this.NotActiveAllies.Add(allyToRemove);
@@ -53,7 +66,7 @@ public class AlliesManager : MonoBehaviour
         this.SaveAlliesStatus();
     }
 
-    public void AddAllyToActiveAllies(Ally allyToAdd)
+    public void AddAllyToActiveAllies(Alleato allyToAdd)
     {
         this.ActiveAllies.Add(allyToAdd);
         this.NotActiveAllies.Remove(allyToAdd);
@@ -61,10 +74,10 @@ public class AlliesManager : MonoBehaviour
         this.SaveAlliesStatus();
     }
 
-    public void AddNewAlly(Ally ally)
+    public void AddNewAlly(Alleato ally)
     {
         this.NotActiveAllies.Add(ally);
-        
+
         this.SaveAlliesStatus();
     }
 
@@ -75,11 +88,11 @@ public class AlliesManager : MonoBehaviour
             Directory.CreateDirectory(saveDirectory);
         }
         
-        var totalAlliesList = new List<List<Ally>> {this.ActiveAllies,this.NotActiveAllies};
+        var totalAlliesList = new List<List<Alleato>> {this.ActiveAllies,this.NotActiveAllies};
         
         File.WriteAllText(savePath,JsonUtility.ToJson(totalAlliesList));
     }
-
+    
     private void LoadAlliesStatus()
     {
         if (!Directory.Exists(saveDirectory))
@@ -87,8 +100,34 @@ public class AlliesManager : MonoBehaviour
             Directory.CreateDirectory(saveDirectory);
             return;
         }
+
+        if (!File.Exists(savePath))
+        {
+            this.SaveAlliesStatus();
+            return;
+        }
         
         var alliesDataJson = File.ReadAllText(savePath);
-        var alliesData = JsonUtility.FromJson<List<List<Ally>>>(alliesDataJson);
+        var alliesData = JsonUtility.FromJson<List<List<Alleato>>>(alliesDataJson);
+    }
+    
+    //-------------------------------CHEATS-----------------------------//
+
+    [RegisterCommand(Help = "Add an ally - args: AllyPrefabName", MinArgCount = 1, MaxArgCount = 1)]
+    private static void AddAlly(CommandArg[] args)
+    {
+        var allyPrefabName = args[0].String;
+
+        foreach (var ally in AlliesManager.Instance.AllPossibleAllies.List)
+        {
+            if (ally.gameObject.name == allyPrefabName)
+            {
+                AlliesManager.Instance.AddNewAlly(ally.gameObject.GetComponent<Alleato>());
+                Debug.Log($"Added ally {allyPrefabName}");
+                return;
+            }
+        }
+        
+        Debug.Log("Ally not found");
     }
 }
